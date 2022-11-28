4161b3fb54b11e338a137595bb54a77e7aa1fa57
Jarmo Vleeshouwer <j.vleeshouwer@youweagency.com>
2022-11-28 20:48:31 +0100

    Improve map design

--- web/components/organisms/Map.tsx
+++ web/components/organisms/Map.tsx
@@ -40,26 +40,86 @@ const Map: FC<MapProps> = ({ locations, onMarkerClick }) => {
         }
     }, [ref, map])
 
-    const paths = new google.maps.Polyline({
-        path: locations.map((location) => {
-            return {
-                lat: location.coords.lat,
-                lng: location.coords.lng,
-            }
-        }),
-        geodesic: true,
-        strokeColor: '#f3936d',
-        strokeOpacity: 1.0,
-        strokeWeight: 5,
-        draggable: false,
-    })
-    // @ts-ignore
-    paths.setMap(map)
+    const visitedLocations = locations.filter((loc) => loc.arrived)
+    const unvisitedLocations = locations.filter((loc) => !loc.arrived)
+
+    if (visitedLocations.length > 0) {
+        const visitedPaths = new google.maps.Polyline({
+            path: visitedLocations.map((location) => {
+                return {
+                    lat: location.coords.lat,
+                    lng: location.coords.lng,
+                }
+            }),
+            geodesic: true,
+            strokeColor: '#f3936d',
+            strokeOpacity: 1.0,
+            strokeWeight: 3,
+            draggable: false,
+        })
+
+        // @ts-ignore
+        visitedPaths.setMap(map)
+    }
+
+    if (unvisitedLocations.length > 0) {
+        const unvisitedPaths = new google.maps.Polyline({
+            path: unvisitedLocations.map((location) => {
+                return {
+                    lat: location.coords.lat,
+                    lng: location.coords.lng,
+                }
+            }),
+            geodesic: true,
+            strokeColor: '#333333',
+            strokeOpacity: 0.4,
+            strokeWeight: 3,
+            draggable: false,
+        })
+
+        // @ts-ignore
+        unvisitedPaths.setMap(map)
+    }
+
+    const currentLocation =
+        visitedLocations.length > 0
+            ? visitedLocations[visitedLocations.length - 1]
+            : null
+
+    const nextLocation =
+        unvisitedLocations.length > 0 ? unvisitedLocations[0] : null
+
+    if (currentLocation && nextLocation) {
+        const nextPath = new google.maps.Polyline({
+            path: [
+                {
+                    lat: currentLocation.coords.lat,
+                    lng: currentLocation.coords.lng,
+                },
+                {
+                    lat: nextLocation.coords.lat,
+                    lng: nextLocation.coords.lng,
+                },
+            ],
+            geodesic: true,
+            strokeColor: '#333333',
+            strokeOpacity: 0.4,
+            strokeWeight: 3,
+            draggable: false,
+        })
+
+        // @ts-ignore
+        nextPath.setMap(map)
+    }
 
     return (
         <>
             <div ref={ref} className="w-full h-[400px]" />
             {locations.map((location: ILocation, index: number) => {
+                const isCurrent = currentLocation
+                    ? location._id === currentLocation._id
+                    : index === 0
+
                 // set the map prop on the child component
                 return cloneElement(
                     <Marker
@@ -69,12 +129,15 @@ const Map: FC<MapProps> = ({ locations, onMarkerClick }) => {
                             lng: location.coords.lng,
                         }}
                         icon={{
-                            url:
-                                location.arrived || index === 0
-                                    ? 'images/MarkerActive.png'
-                                    : 'images/Marker.png',
-                            scaledSize: new google.maps.Size(25, 34),
+                            url: isCurrent
+                                ? 'images/MarkerActive.png'
+                                : 'images/Marker.png',
+                            scaledSize: new google.maps.Size(
+                                isCurrent ? 37 : 25,
+                                isCurrent ? 51 : 34
+                            ),
                         }}
+                        opacity={location.arrived ? 1 : 0.7}
                         clickable={true}
                         id={location._id}
                         onClick={handleMarkerClick}
